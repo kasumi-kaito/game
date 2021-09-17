b1f399a48238c72bf2eb3be94c4460bdad96e470
kasumi-kaito <79437914+kasumi-kaito@users.noreply.github.com>
2021-09-17 11:23:28 +0900

    Add files via upload

---
+++ real-escape-game-v5/a.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+      r();
+      load();
+}
+function r(){
+      i = 3600; }
+var a = function(){
+          i --;
+      u = document.getElementById('p')   
+u.innerHTML=　'残り'　+ i + '秒';
+     if(i < 600){
+     u.className = 't'
+     }
+     if(i==0){ 
+           clearInterval(g);
+      }
+            localStorage.setItem('残り時間',i)   
+}
+ g = setInterval(a,1000)
+      function load(){
+            i = localStorage.getItem('残り時間',i)
+            }
+     　function y(){
+            clearInterval(g);
+            localStorage.clear()
+            i = 3600;
+            localStorage.setItem('残り時間',i)    
+      }
